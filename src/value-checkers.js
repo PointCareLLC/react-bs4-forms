acc360458c3860d659fca02d8ed38f366843c2d2
Eric Zingeler <eric@ezinterweb.com>
2016-03-25 10:49:02 -0700

    value-chekcers unit tests

--- src/value-checkers.js
+++ src/value-checkers.js
@@ -1,14 +1,28 @@
 
-export function validCheck(value, required, func) {
+/**
+    * Test if field value is valid
+    * @param {(string|number)} [value] - field value
+    * @param {bool} [required=false] - is field required
+    * @param {func} [func] - custom field validation function
+*/
+export function validCheck(value, required = false, func) {
 
     const isValid = func ? func(value) : null
 
-    if (isValid === false) return false
+    if (isValid !== null) return isValid
+
     if (required === false) return true
-    if (value === '' || value === null || value === undefined) return false
+
+    if (emptyCheck(value) === true) return false
+
     return true
 }
 
+
+/**
+    * Test if value is empty. Note: false & 0 are considered non-empty
+    * @param {*} [value] - field value
+*/
 export function emptyCheck(value) {
     return (value === '' || value === null || value === undefined) ? true : false
 }
