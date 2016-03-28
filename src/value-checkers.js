6c16eef8f76ac48ce4c65488b04fb40d90828c38
Eric Zingeler <eric@ezinterweb.com>
2016-03-27 18:12:15 -0700

    jsdoc additions. removed field-mixin test

--- src/value-checkers.js
+++ src/value-checkers.js
@@ -4,6 +4,7 @@
     * @param {(string|number)} [value] - field value
     * @param {bool} [required=false] - is field required
     * @param {func} [func] - custom field validation function
+    * @returns {bool}
 */
 export function validCheck(value, required = false, func) {
 
@@ -22,6 +23,7 @@ export function validCheck(value, required = false, func) {
 /**
     * Test if value is empty. Note: false & 0 are considered non-empty
     * @param {*} [value] - field value
+    * @returns {bool}
 */
 export function emptyCheck(value) {
     return (value === '' || value === null || value === undefined) ? true : false
