670bc56d3e2baac4f4f144c479920eec8ab0cc7f
Eric Zingeler <ericzingeler@Erics-MacBook-Pro.local>
2016-06-24 12:59:58 -0700

    style seperation

--- example/src/index.js
+++ example/src/index.js
@@ -1,18 +1,21 @@
 
-import './style/index.scss'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import FormView from './Form-View.jsx'
+import './style/index.scss';
+import '../../src/style/index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FormView from './Form-View.jsx';
 
 ReactDOM.render(
-    <div className="container">
-        <div className="row">
-            <div className="col-md-8 col-md-offset-2">
-                <h4>react-bs4-forms</h4>
-                <hr />
-                <FormView />
-            </div>
-        </div>
-    </div>,
-    document.getElementById('root')
-)
+	<div className="container">
+		<div className="row">
+			<div className="col-md-8 col-md-offset-2">
+				<h4>react-bs4-forms</h4>
+				<hr />
+				<div className="bs4-forms">
+					<FormView />
+				</div>
+			</div>
+		</div>
+	</div>,
+	document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
 
-var path = require('path')
+var path = require('path');
 
 var args = {
-    port: 3000,
-    entryPath: 'example/src/index',
-    outputPath: 'example/public'
-}
+	port: 3000,
+	entryPath: 'example/src/index',
+	outputPath: 'example/public'
+};
 
 module.exports = {
-    entry: [
-        'webpack-dev-server/client?http://0.0.0.0:' + args.port,
-        'webpack/hot/only-dev-server',
-        path.resolve(__dirname, args.entryPath)
-    ],
-    output: {
-        path: path.resolve(__dirname, args.outputPath),
-        filename: "bundle.js"
-    },
-    devServer: {
-        port: args.port,
-        contentBase: args.outputPath
-    },
-    resolve: {
-        root: path.resolve(__dirname),
-        extensions: ['', '.js', '.jsx']
-    },
-    devtool: 'eval-source-map',
-    debug: true,
-    module: {
-        loaders: [
-            {
-                test: /\.scss$/,
-                loaders: ["style", "css", "sass"]
-            },
-            {
-                test: /\.jsx?$/,
-                loaders: ['react-hot', 'babel-loader?cacheDirectory'],
-                exclude: /node_modules/
-            }
-        ]
-    }
-}
+	entry: [
+		'webpack-dev-server/client?http://0.0.0.0:' + args.port,
+		'webpack/hot/only-dev-server',
+		path.resolve(__dirname, args.entryPath)
+	],
+	output: {
+		path: path.resolve(__dirname, args.outputPath),
+		filename: 'bundle.js'
+	},
+	devServer: {
+		port: args.port,
+		contentBase: args.outputPath
+	},
+	resolve: {
+		root: path.resolve(__dirname),
+		extensions: ['', '.js', '.jsx']
+	},
+	devtool: 'eval-source-map',
+	debug: true,
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				loaders: ['style', 'css', 'sass']
+			},
+			{
+				test: /\.jsx?$/,
+				loaders: ['react-hot', 'babel-loader?cacheDirectory'],
+				exclude: /node_modules/
+			}
+		]
+	}
+};
