51e0620fca894ee30edfb335011b407b6ed9af9a
Ahsan Zahid <pie.is.forever@gmail.com>
2016-07-01 15:04:09 -0700

    Wrapped fields in HOCs

--- src/fields/RadioGroup.jsx
+++ src/fields/RadioGroup.jsx
@@ -1,6 +1,6 @@
-
 import radioGroupHoc from '../add-ons/radio-group-hoc';
 import fieldHoc from '../add-ons/field-hoc';
 import Radio from '../elements/Radio';
+import validatedFieldHoc from '../add-ons/validated-field-hoc';
 
-export default fieldHoc(radioGroupHoc(Radio));
+export default validatedFieldHoc(fieldHoc(radioGroupHoc(Radio)));
