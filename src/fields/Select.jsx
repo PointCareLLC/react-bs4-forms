62be560049b76bbd02ef329f894fcfb2ccc71955
Eric Zingeler <ericzingeler@Erics-MacBook-Pro.local>
2016-07-01 11:08:46 -0700

    covered fields input, textarea and select to hoc

--- src/fields/Select.jsx
+++ src/fields/Select.jsx
@@ -1,41 +1,6 @@
 
-import React from 'react';
-import Label from '../elements/Label';
-import Fieldset from '../elements/Fieldset';
+
 import Select from '../elements/Select';
+import fieldHoc from '../add-ons/field-hoc';
 
-export default React.createClass({
-	propTypes: {
-		name: React.PropTypes.string.isRequired,
-		onUpdate: React.PropTypes.func.isRequired,
-		label: React.PropTypes.string.isRequired,
-		labelHide: React.PropTypes.bool,
-		options: React.PropTypes.array.isRequired,
-		placeholder: React.PropTypes.string,
-		value: React.PropTypes.string,
-		valid: React.PropTypes.bool
-	},
-	render,
-	shouldComponentUpdate
-});
-
-function render() {
-
-	const { label, valid, ...elProps } = this.props;
-
-	return (
-        <Fieldset valid={this.props.valid}>
-            <Label
-				id={this.props.id || this.props.name}
-				text={this.props.label}
-				required={this.props.required}
-				hide={this.props.labelHide}
-			/>
-			<Select {...elProps} />
-        </Fieldset>
-    );
-}
-
-function shouldComponentUpdate(nextProps) {
-	return (nextProps.value !== this.props.value || nextProps.valid !== this.props.valid || nextProps.disabled !== this.props.disabled);
-}
+export default fieldHoc(Select);
--- src/fields/Textarea.jsx
+++ src/fields/Textarea.jsx
@@ -1,39 +1,5 @@
 
-import React from 'react';
-import Label from '../elements/Label';
-import Fieldset from '../elements/Fieldset';
 import Textarea from '../elements/Textarea';
+import fieldHoc from '../add-ons/field-hoc';
 
-export default React.createClass({
-	propTypes: {
-		name: React.PropTypes.string.isRequired,
-		onUpdate: React.PropTypes.func.isRequired,
-		label: React.PropTypes.string.isRequired,
-		labelHide: React.PropTypes.bool,
-		value: React.PropTypes.string,
-		valid: React.PropTypes.bool
-	},
-	render,
-	shouldComponentUpdate
-});
-
-function render() {
-
-	const { label, valid, ...elProps } = this.props;
-
-	return (
-		<Fieldset valid={this.props.valid}>
-			<Label
-				id={this.props.id || this.props.name}
-				text={this.props.label}
-				required={this.props.required}
-				hide={this.props.labelHide}
-			/>
-		<Textarea {...elProps} />
-		</Fieldset>
-    );
-}
-
-function shouldComponentUpdate(nextProps) {
-	return (nextProps.value !== this.props.value || nextProps.valid !== this.props.valid || nextProps.disabled !== this.props.disabled);
-}
+export default fieldHoc(Textarea);
