051f53347b7ffd2f1e0f4baa5b032882287faf53
Eric Zingeler <ericzingeler@Erics-MacBook-Pro.local>
2016-07-15 12:13:02 -0700

    added maskOut to masked input field

--- src/fields/Masked-Input.jsx
+++ src/fields/Masked-Input.jsx
@@ -5,17 +5,27 @@ import Input from '../elements/Input';
 import maskHoc from '../add-ons/input-mask-hoc';
 import fieldHoc from '../add-ons/field-hoc';
 import validatedFieldHoc from '../add-ons/validated-field-hoc';
+import { toPattern } from '../helpers/mask';
 
 const InputWithMask = validatedFieldHoc(fieldHoc(maskHoc(Input)));
 
 export default React.createClass({
 	propTypes: {
 		mask: React.PropTypes.string.isRequired,
-		validator: React.PropTypes.func
+		maskOut: React.PropTypes.string,
+		validator: React.PropTypes.func,
+		value: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+		onUpdate: React.PropTypes.func.isRequired
+	},
+	componentWillMount() {
+		this.maskedValue = toPattern(this.props.value || '', this.props.mask);
 	},
 	render() {
+
+		const {maskOut, value, onUpdate, ...elProps} = this.props;
+
 		return (
-			<InputWithMask {...this.props} validator={this.validator} />
+			<InputWithMask {...elProps} validator={this.validator} value={this.maskedValue} onUpdate={this.handleUpdate} />
 		);
 	},
 	validator(v) {
@@ -25,5 +35,13 @@ export default React.createClass({
 		}
 
 		return false;
+	},
+	handleUpdate(name, value, valid) {
+
+		this.maskedValue = value;
+
+		let outValue = this.props.maskOut ? toPattern(value || '', this.props.maskOut) : value;
+
+		return this.props.onUpdate(name, outValue, valid);
 	}
 });
