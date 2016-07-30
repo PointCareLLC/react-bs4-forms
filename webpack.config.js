ed95f233c4da2135ce4396c0436553769109d588
eezing <eric@ezinterweb.com>
2016-07-30 15:58:54 -0700

    clean up styles and update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,70 @@
 
-var path = require('path');
+const path = require('path');
 
-var args = {
-	port: 3000,
-	entryPath: 'example/src/index',
-	outputPath: 'example/public'
-};
+const PORT = process.env.PORT || 3000;
+
+const isVerbose = process.argv.includes('--verbose');
+const isDebug = process.argv.includes('--debug');
 
 module.exports = {
 	entry: [
-		'webpack-dev-server/client?http://0.0.0.0:' + args.port,
+		'webpack-dev-server/client?http://0.0.0.0:' + PORT,
 		'webpack/hot/only-dev-server',
-		path.resolve(__dirname, args.entryPath)
+		path.join(__dirname, './demo/src')
 	],
 	output: {
-		path: path.resolve(__dirname, args.outputPath),
+		path: path.join(__dirname, './demo/public'),
 		filename: 'bundle.js'
 	},
 	devServer: {
-		port: args.port,
-		contentBase: args.outputPath
+		port: PORT,
+		contentBase: path.join(__dirname, './demo/public'),
+		stats: {
+			colors: true,
+			reasons: isDebug,
+			hash: isVerbose,
+			version: isVerbose,
+			timings: true,
+			chunks: isVerbose,
+			chunkModules: isVerbose,
+			cached: isVerbose,
+			cachedAssets: isVerbose
+		}
 	},
 	resolve: {
-		root: path.resolve(__dirname),
 		extensions: ['', '.js', '.jsx']
 	},
-	devtool: 'eval-source-map',
-	debug: true,
+	debug: isDebug,
+	devtool: isDebug ? 'source-map' : false,
 	module: {
-		loaders: [
-			{
-				test: /\.scss$/,
-				loaders: ['style', 'css', 'sass']
-			},
-			{
-				test: /\.jsx?$/,
-				loaders: ['react-hot', 'babel-loader?cacheDirectory'],
-				exclude: /node_modules/
-			}
-		]
+		loaders: [{
+			test: /\.s[ac]ss$/,
+			loaders: ['style', 'css', 'resolve-url', 'sass?sourceMap']
+		},
+		{
+			test: /\.jsx?$/,
+			loader: 'react-hot!babel?cacheDirectory=true',
+			exclude: /node_modules/
+		},
+		{
+			test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'url?limit=10000&mimetype=application/font-woff'
+		},
+		{
+			test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'url?limit=10000&mimetype=application/font-woff2'
+		},
+		{
+			test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'url?limit=10000&mimetype=application/octet-stream'
+		},
+		{
+			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'file'
+		},
+		{
+			test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'url?limit=10000&mimetype=image/svg+xml'
+		}]
 	}
 };
